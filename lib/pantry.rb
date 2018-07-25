85d776715d5ed8bc8a57395aa3384cf2fce74754
mmbensalah <mmbensalah@gmail.com>
2018-07-25 14:56:50 -0600

    Miriam

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,3 +1,27 @@
+require 'pry'
 class Pantry
+  attr_reader :stock, :shopping_list
 
+  def initialize
+    @stock = {}
+    @shopping_list = {}
+  end
+
+  def stock_check(food)
+    if @stock.include?(food)
+      @stock[food]
+    else
+      0
+    end
+  end
+
+  def restock(food, number)
+    @stock[food] = number
+  end
+
+  def add_to_shopping_list(recipe)
+    @shopping_list = recipe.ingredients
+  end
+  #got stuck on Iteration 2 - couldn't figure out how to update hash vs replace it.
+  #Tried .inject to no success 
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -1,7 +1,59 @@
 require './lib/pantry'
+require './lib/recipe'
 require 'minitest/autorun'
 require 'minitest/pride'
 
 class PantryTest < Minitest::Test
+
+  def test_it_exists
+    pantry = Pantry.new
+    assert_instance_of Pantry, pantry
+  end
+
+  def test_for_0_in_pantry
+    pantry = Pantry.new
+    assert_equal 0, pantry.stock_check("cheese")
+  end
+
+  def test_restock
+    pantry = Pantry.new
+    pantry.restock("Cheese", 10)
+    pantry.stock_check("Cheese")
+
+    assert_equal 10, pantry.stock_check("Cheese")
+  end
+
+  def test_restock_adds_food_quantity
+    pantry = Pantry.new
+    pantry.restock("Cheese", 10)
+    pantry.restock("Cheese", 20)
+
+    assert_equal 30, pantry.stock_check("Cheese")
+  end
+
+  def test_returns_shopping_list
+    pantry = Pantry.new
+    r = Recipe.new("Cheese Pizza")
+    r.add_ingredient("Cheese", 20)
+    r.add_ingredient("Flour", 20)
+    pantry.add_to_shopping_list(r)
+
+    assert_equal [{"Cheese" => 20, "Flour" => 20}], [pantry.shopping_list]
+  end
+
+  def test_returns_shopping_list_for_multiple_recipes
+    pantry = Pantry.new
+    r = Recipe.new("Cheese Pizza")
+    r.add_ingredient("Cheese", 20)
+    r.add_ingredient("Flour", 20)
+    pantry.add_to_shopping_list(r)
+    r = Recipe.new("Spaghetti")
+    r.add_ingredient("Spaghetti Noodles", 10)
+    r.add_ingredient("Marinara Sauce", 10)
+    r.add_ingredient("Cheese", 5)
+    pantry.add_to_shopping_list(r)
+
+    assert_equal [{"Cheese" => 25, "Flour" => 20, "Spaghetti Noodles" => 10, "Marinara Sauce" => 10}], pantry.shopping_list
+  end
 
 end
